9bdbf5a83a381251006917d72a456362988e589c
Carlo Munguia <carlomunguia@carlos-air.home>
2019-08-27 12:34:23 -0500

    integrated skills with model generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
+# This is the controller for the various Pages.
 class PagesController < ApplicationController
   def home
+    @posts = Blog.all
+    @skills = Skill.all
   end
 
   def about
