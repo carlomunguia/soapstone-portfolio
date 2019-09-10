0b7965e0c98733c59e7a5bff7e6236c21b2cd16e
Carlo Munguia <carlo.munguia.82@gmail.com>
2019-09-10 16:19:31 -0500

    created placeholder module to reuse through codebase

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,6 +2,7 @@
 
 # This is the Portfolio controller
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -13,9 +14,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= 'http://placehold.it/600x400'
-    self.thumb_image ||= 'http://placehold.it/350x200'
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
-
-self.main_image = 'http://placehold.it/600x400' if self.main_image.nil?
