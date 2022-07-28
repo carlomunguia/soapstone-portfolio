7210bacb7bfe8a94b00a28026d35dafa7a06c3ea
Carlo Munguia <cmunguia@denimgroup.com>
2022-07-27 21:06:02 -0500

    Soapstone 99
    updated portfolios controller by changing out direct inline parms to separate method

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -15,15 +15,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item =
-      Portfolio.new(
-        params.require(:portfolio).permit(
-          :title,
-          :subtitle,
-          :body,
-          technologies_attributes: [:name]
-        )
-      )
+    @portfolio_item = Portfolio.new(portfolio_params)
     respond_to do |format|
       if @portfolio_item.save
         format.html do
@@ -44,9 +36,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(
-           params.require(:portfolio).permit(:title, :subtitle, :body)
-         )
+      if @portfolio_item.update(portfolio_params)
         format.html do
           redirect_to portfolios_path,
                       notice: "The record successfully updated."
@@ -70,6 +60,17 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: "Record was removed!" }
     end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(
+      :title,
+      :subtitle,
+      :body,
+      technologies_attributes: [:name]
+    )
   end
 
   # This is a dummy-class that is reusable through the code
